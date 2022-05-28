0a2a91f38d5db5bfdc5397f592d78d13a30808b9
sirgak <life_4_ever@mail.ru>
2022-05-28 20:29:41 +0300

    test
    text

---
+++ homework007/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace homework007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            int howManyTimes;
+            Console.Write("Введите текст: ");
+            text = Convert.ToString(Console.ReadLine());
+            Console.Write("Количество повторов Текста: ");
+            howManyTimes = Convert.ToInt32(Console.ReadLine());
+            while (howManyTimes > 0)
+            { Console.WriteLine(text); howManyTimes--;}
+        }
+    }
+}
